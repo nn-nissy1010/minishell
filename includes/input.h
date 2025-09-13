918c9759357e019cc6de945b8c2afa1bd121da75
nn-nissy1010 <nn-nissy1010@nn-nissy1010noMacBook-Pro.local>
2025-09-13 16:15:34 +0900

    クォート閉じ忘れなどの対処

--- includes/input.h
+++ includes/input.h
@@ -6,7 +6,7 @@
 /*   By: nnishiya <nnishiya@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 17:11:30 by nnishiya          #+#    #+#             */
-/*   Updated: 2025/09/12 17:17:17 by nnishiya         ###   ########.fr       */
+/*   Updated: 2025/09/13 16:07:41 by nnishiya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,27 @@
 #include <signal.h>
 #include <stdlib.h>
 
+extern int g_exit_status;
+
+typedef enum e_more {
+    MORE_NONE = 0,
+    MORE_QUOTE_S,
+    MORE_QUOTE_D,
+    MORE_OP
+} t_more;
+
 int repl(void);
+
+void install_signal_handlers(void);
+
+char *read_full_command_line(char *first_line);
+
+int	handle_no_quote(char c, int *q, size_t *i, const char *s);
+int	handle_single_quote(char c, int *q);
+int	handle_double_quote(char c, int *q, size_t i, const char *s);
+t_more need_more_input(const char *s);
+
+int ft_isspace(unsigned char c);
+int is_blank_line(const char *s);
+char	*ft_strjoin_3word(char *s1, const char *s2, const char *s3);
+void	print_syntax_error(const char *msg);
--- src/input/repl_utils/syntax_check_utils.c
+++ src/input/repl_utils/syntax_check_utils.c
@@ -1,21 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   syntax_check_utils.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nnishiya <nnishiya@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/12 17:11:46 by nnishiya          #+#    #+#             */
-/*   Updated: 2025/09/13 16:09:15 by nnishiya         ###   ########.fr       */
+/*   Created: 2025/09/13 15:59:46 by nnishiya          #+#    #+#             */
+/*   Updated: 2025/09/13 16:01:28 by nnishiya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int g_exit_status = 0;
-
-int	main(void)
+int	handle_no_quote(char c, int *q, size_t *i, const char *s)
 {
-	repl();
-	return (0);
+	if (c == '\'' || c == '"')
+		*q = c;
+	else if (c == '\\' && s[*i + 1])
+		(*i)++;
+	return (*q);
+}
+
+int	handle_single_quote(char c, int *q)
+{
+	if (c == '\'')
+		*q = 0;
+	return (*q);
+}
+
+int	handle_double_quote(char c, int *q, size_t i, const char *s)
+{
+	if (c == '"' && (i == 0 || s[i - 1] != '\\'))
+		*q = 0;
+	return (*q);
 }
