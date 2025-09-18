e2ccec66d53dd14bc40232a0a2d8c429c95d8267
nn-nissy1010 <nn-nissy1010@nn-nissy1010noMacBook-Pro.local>
2025-09-18 20:13:17 +0900

    トークン構造体の定義と、操作関数の作成

--- includes/lexer.h
+++ includes/lexer.h
@@ -1,11 +1,63 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   laxer.h                                            :+:      :+:    :+:   */
+/*   lexer.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nnishiya <nnishiya@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 17:13:01 by nnishiya          #+#    #+#             */
-/*   Updated: 2025/09/12 17:13:02 by nnishiya         ###   ########.fr       */
+/*   Updated: 2025/09/18 19:47:50 by nnishiya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef LAXER
+# define LAXER
+
+typedef enum e_tokentype {
+    TOK_ARG,
+    TOK_PIPE,
+    TOK_REDIR_IN,
+    TOK_REDIR_OUT,
+    TOK_REDIR_APPEND,
+    TOK_HEREDOC,
+    TOK_AND_IF,
+    TOK_OR_IF,
+    TOK_LPAR,
+    TOK_RPAR,
+    TOK_EOF
+} t_tokentype;
+
+typedef struct s_arg {
+    char   *raw;
+    char  **items;
+    size_t  n_items;
+    int     has_param;
+    int     saw_quote;
+    int     has_unq_glob;
+    int     expanded;
+} t_arg;
+
+typedef struct s_heredoc {
+    int heredoc_quoted;
+} t_heredoc;
+
+
+typedef struct s_token {
+    t_tokentype type;
+    union {
+        t_arg     arg;
+        t_heredoc heredoc;
+    } u;
+    struct s_token *next;
+} t_token;
+
+t_token *new_token_n(t_tokentype type, const char *s, size_t n);
+t_token *new_token(t_tokentype type, const char *s);
+void tok_push(t_token **head, t_token **tail, t_token *node);
+void free_tokens(t_token *t);
+
+
+
+
+
+#endif
---
+++ src/laxer/laxer.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   laxer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnishiya <nnishiya@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/18 19:36:28 by nnishiya          #+#    #+#             */
+/*   Updated: 2025/09/18 20:06:20 by nnishiya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_token *new_token_n(t_tokentype type, const char *s, size_t n)
+{
+    t_token *t;
+
+    t = malloc(sizeof(*t));
+    if (!t)
+        return (perror("malloc_error"), NULL);
+    t->type = type;
+    t->next = NULL;
+    if (type == TOK_ARG)
+    {
+        if (s && n)
+            t->u.arg.raw = ft_strndup(s, n);
+        else
+            t->u.arg.raw = ft_strdup("");
+        t->u.arg.items = NULL;
+        t->u.arg.n_items = 0;
+        t->u.arg.has_param = 0;
+        t->u.arg.saw_quote = 0;
+        t->u.arg.has_unq_glob = 0;
+        t->u.arg.expanded = 0;
+    }
+    else if (type == TOK_HEREDOC)
+        t->u.heredoc.heredoc_quoted = 0;
+    return (t);
+}
+
+t_token *new_token(t_tokentype type, const char *s)
+{
+    if (!s)
+        return new_token_n(type, NULL, 0);
+    return new_token_n(type, s, ft_strlen(s));
+}
+
+void tok_push(t_token **head, t_token **tail, t_token *node)
+{
+    if (!node)
+        return;
+    if (!*head)
+        *head = node;
+    else
+        (*tail)->next = node;
+    *tail = node;
+}
+
+void free_tokens(t_token *t)
+{
+    t_token *n;
+    size_t   i;
+
+    while (t)
+    {
+        n = t->next;
+        if (t->type == TOK_ARG)
+        {
+            free(t->u.arg.raw);
+            if (t->u.arg.items)
+            {
+                i = 0;
+                while (i < t->u.arg.n_items)
+                {
+                    free(t->u.arg.items[i]);
+                    i++;
+                }
+                free(t->u.arg.items);
+            }
+        }
+        free(t);
+        t = n;
+    }
+}
