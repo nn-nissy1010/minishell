0f13147a0ccc0dce265498426d24db653f026ee2
naoki <nnishiya@student.42tokyo.jp>
2025-10-12 12:34:36 +0900

    norm修正

--- src/exec/exec_cmd/child/exec_cmd_child.c
+++ src/exec/exec_cmd/child/exec_cmd_child.c
@@ -6,39 +6,39 @@
 /*   By: nnishiya <nnishiya@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 22:15:50 by tkuwahat          #+#    #+#             */
-/*   Updated: 2025/10/12 12:29:35 by nnishiya         ###   ########.fr       */
+/*   Updated: 2025/10/12 12:33:45 by nnishiya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int    run_builtin_child(t_cmd *c)
+int	run_builtin_child(t_cmd *c)
 {
-    char    **av;
+	char	**av;
 
-    if (!c || !c->argv || !c->argv[0])
-        return (0);
-    av = c->argv;
-    if (ft_strcmp(av[0], "echo") == 0)
-        return (bi_echo(av));
-    if (ft_strcmp(av[0], "pwd") == 0)
-        return (bi_pwd(av));
-    if (ft_strcmp(av[0], "env") == 0)
-        return (bi_env(av));
-    if (ft_strcmp(av[0], "cd") == 0)
-        return (bi_cd(av));
-    if (ft_strcmp(av[0], "export") == 0)
-        return (bi_export(av));
-    if (ft_strcmp(av[0], "unset") == 0)
-        return (bi_unset(av));
-    if (ft_strcmp(av[0], "exit") == 0)
-    {
-        if(c->argc == 1 || c->argc == 2 || exit_validate(av[1]) == 0)
-            return (bi_exit(av));
-        else
-            return (write(STDOUT_FILENO, "exit: too many arguments\n", 25), 1);
-    }
-    return (127);
+	if (!c || !c->argv || !c->argv[0])
+		return (0);
+	av = c->argv;
+	if (ft_strcmp(av[0], "echo") == 0)
+		return (bi_echo(av));
+	if (ft_strcmp(av[0], "pwd") == 0)
+		return (bi_pwd(av));
+	if (ft_strcmp(av[0], "env") == 0)
+		return (bi_env(av));
+	if (ft_strcmp(av[0], "cd") == 0)
+		return (bi_cd(av));
+	if (ft_strcmp(av[0], "export") == 0)
+		return (bi_export(av));
+	if (ft_strcmp(av[0], "unset") == 0)
+		return (bi_unset(av));
+	if (ft_strcmp(av[0], "exit") == 0)
+	{
+		if (c->argc == 1 || c->argc == 2 || exit_validate(av[1]) == 0)
+			return (bi_exit(av));
+		else
+			return (write(STDOUT_FILENO, "exit: too many arguments\n", 25), 1);
+	}
+	return (127);
 }
 
 int	is_builtin_any(const t_cmd *c)
@@ -48,13 +48,10 @@ int	is_builtin_any(const t_cmd *c)
 	if (!c || !c->argv || !c->argv[0])
 		return (0);
 	b = c->argv[0];
-	if (ft_strcmp(b, "cd") == 0
-		|| ft_strcmp(b, "export") == 0
-		|| ft_strcmp(b, "unset") == 0
-		|| ft_strcmp(b, "exit") == 0
-		|| ft_strcmp(b, "echo") == 0
-		|| ft_strcmp(b, "pwd") == 0
-		|| ft_strcmp(b, "env") == 0)
+	if (ft_strcmp(b, "cd") == 0 || ft_strcmp(b, "export") == 0 || ft_strcmp(b,
+			"unset") == 0 || ft_strcmp(b, "exit") == 0 || ft_strcmp(b,
+			"echo") == 0 || ft_strcmp(b, "pwd") == 0 || ft_strcmp(b,
+			"env") == 0)
 		return (1);
 	return (0);
 }
